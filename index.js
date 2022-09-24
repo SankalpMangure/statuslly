77544ed08e80eff88a6803168cffae2cd70d6a8c
Sankalp Mangure <106030279+SankalpMangure@users.noreply.github.com>
2022-09-24 19:36:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.listen(PORT, () => {
   console.log("connected to db & listening on port 4000");
 });
 
+app.get("/", function (req, res) {
+  res.send("Shree gurudev datta");
+  res.end();
+});
+
 // middleware
 // app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
@@ -22,11 +27,6 @@ app.use(express.static(__dirname));
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
-});
-
-app.get("/", function (req, res) {
-  res.send("Shree gurudev datta");
-  res.end();
 });
 
 // routes
